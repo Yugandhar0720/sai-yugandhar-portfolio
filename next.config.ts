10c83324d59ff2ac61add40eb5b49d34ef2e0fb2
Yugandhar0720 <95173060+Yugandhar0720@users.noreply.github.com>
2025-11-12 10:44:53 +0530

    Update next.config.ts for production settings
    Configured Next.js for production deployment with GitHub Pages.

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
-import type { NextConfig } from "next";
+/** @type {import('next').NextConfig} */
+const isProd = process.env.NODE_ENV === 'production';
+const repo = 'sai-yugandhar-portfolio'; // your repo name on GitHub
 
-const nextConfig: NextConfig = {
-  /* config options here */
+const nextConfig = {
+  output: 'export',               // enable static export
+  images: { unoptimized: true },  // disable image optimizer for GH Pages
+  basePath: isProd ? `/${repo}` : '',
+  assetPrefix: isProd ? `/${repo}/` : '',
 };
 
 export default nextConfig;
